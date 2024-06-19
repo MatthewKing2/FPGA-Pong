c3d321f49c3f6f3432ea37500a90f06ca8cfaf9f
MatthewKing2 <matthewjesseking@gmail.com>
2024-06-19 12:59:04 -0500

    Mostly fixed ball behavior (paddle collision bugs)

--- ballBehavior.v
+++ ballBehavior.v
@@ -26,22 +26,17 @@ module ballBehavior #(
     parameter lowerBound = 480-buffer;
     parameter rightBound = 640-buffer;
     parameter leftBound  = 0+buffer;
-    reg [9:0] r_x_pos = (480/2) - (BALL_WIDTH/2);
-    reg [9:0] r_y_pos = (640/2) - (BALL_HEIGHT/2);
+    parameter ballStartX = (640/2) - (BALL_WIDTH/2);
+    parameter ballStartY = (480/2) - (BALL_HEIGHT/2);
+    reg [9:0] r_x_pos = ballStartX;
+    reg [9:0] r_y_pos = ballStartY; 
     reg       r_deltaX_sign = 0;   // 0 = Right, 1 = Left
     reg       r_deltaY_sign = 0;   // 0 = Up, 1 = Down
     reg [1:0] r_gameStart = 0;
     reg [1:0] r_collieded = 0; 
+    reg       r_gameReset = 0;
 
-    // See if someone scored
-    always @( posedge i_CLK ) begin 
-        if(r_x_pos < leftBound)
-            o_p2_scored <= 1;
-        if(r_x_pos + BALL_WIDTH > rightBound)
-            o_p1_scored <= 1;
-    end
-
-    // One Collide, change direction
+    // Handle Colliding with Walls and Paddles
     always @( posedge i_CLK ) begin
         if(r_collieded == 0) begin
             // Top and Bottom
@@ -49,40 +44,59 @@ module ballBehavior #(
                 r_deltaY_sign <= ~r_deltaY_sign;
                 r_collieded <= 1;
             end
-            // Right and Left (for testing)
-            if((r_x_pos <= leftBound) || (r_x_pos+BALL_WIDTH >= rightBound)) begin
-                r_deltaX_sign <= ~r_deltaX_sign;
-                r_collieded <= 1;
-            end
             // Left Paddle (Player 1)
-            else if((r_x_pos >= P1_X_POS-BALL_WIDTH ) && (r_x_pos <= P1_X_POS+PADDLE_WIDTH) && 
-            (r_y_pos >= i_p1_y_pos + BALL_HEIGHT ) && (r_y_pos <= i_p1_y_pos + PADDLE_HEIGHT)) begin
+            if((r_x_pos <= P1_X_POS+PADDLE_WIDTH) && 
+                    (r_y_pos >= i_p1_y_pos + BALL_HEIGHT ) && 
+                    (r_y_pos <= i_p1_y_pos + PADDLE_HEIGHT)) begin
                 r_deltaX_sign <= ~r_deltaX_sign;
                 r_collieded <= 1;
             end
             // Right Paddle (Player 2)
-            else if((r_x_pos >= P2_X_POS-BALL_WIDTH ) && (r_x_pos <= P2_X_POS+PADDLE_WIDTH) &&
-            (r_y_pos >= i_p2_y_pos + BALL_HEIGHT ) && (r_y_pos <= i_p2_y_pos + PADDLE_HEIGHT)) begin
+            else if((r_x_pos >= P2_X_POS-BALL_WIDTH ) && 
+                    (r_y_pos >= i_p2_y_pos + BALL_HEIGHT ) && 
+                    (r_y_pos <= i_p2_y_pos + PADDLE_HEIGHT)) begin
                 r_deltaX_sign <= ~r_deltaX_sign;
                 r_collieded <= 1;
             end
+            // Left Wall
+            else if((r_x_pos < leftBound) && r_gameReset == 0) begin
+                o_p1_scored <= 1;
+                r_gameReset <= 1;
+            end
+            // Right Wall
+            else if((r_x_pos + BALL_WIDTH > rightBound) && r_gameReset == 0) begin
+                o_p2_scored <= 1;
+                r_gameReset <= 1;
+            end
+            else begin 
+                o_p1_scored <= 0;
+                o_p2_scored <= 0;
+                r_gameReset <= 0;   // Flags that means move normally
+            end
         end
+        // Wait 2 cycles after a collision before being able to collid again
         else if(r_collieded == 1)
             r_collieded <= 2;
         else
             r_collieded <= 0;
     end
     
-    // Move the ball
+    // Move the ball (handles game reset movment)
     always @( posedge i_CLK ) begin 
-        if(r_deltaX_sign == 0)
-            r_x_pos <= r_x_pos + SPEED;
-        else
-            r_x_pos <= r_x_pos - SPEED;
-        if(r_deltaY_sign == 1)
-            r_y_pos <= r_y_pos + SPEED;
-        else
-            r_y_pos <= r_y_pos - SPEED;
+        if(r_gameReset == 0) begin
+            if(r_deltaX_sign == 0)
+                r_x_pos <= r_x_pos + SPEED;
+            else
+                r_x_pos <= r_x_pos - SPEED;
+            if(r_deltaY_sign == 1)
+                r_y_pos <= r_y_pos + SPEED;
+            else
+                r_y_pos <= r_y_pos - SPEED;
+        end
+        else begin 
+            r_x_pos <= ballStartX;
+            r_y_pos <= ballStartY;
+        end
     end
 
     // See if a valid key (for the ball) was pressed & store it
